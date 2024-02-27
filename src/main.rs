a7ba18721fcf17ee94f8292d5bf61c85fe427285
jianghao <jianghao@haplox.com>
2024-02-27 17:49:14 +0800

    fix: 更新main

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,18 @@ use minigrep::run;
 use minigrep::Config;
 
 fn main() {
-    let args = env::args(); // Args 实现了 Iterator
-    let config = Config::build(args).unwrap();
+    // let args = env::args(); // Args 实现了 Iterator
+    // let config = Config::build(args).unwrap();
 
-    run(config).unwrap();
+    // run(config).unwrap();
     // unwrap_or_else 错误处理
-    // let config = Config::build(env::args()).unwrap_or_else(|err| {
-    //     println!("输入错误: {err}");
-    //     process::exit(1); //退出代码终止当前进程
-    // });
-
-    // let config = Config::build(env::args()).unwrap_or(|e| {
-    //     println!("{e}");
-    //     process::exit(1);
-    // });
-
-    // Err(str) 错误信息
-    // if let Err(e) = jiang_mini_grep::run(config) {
-    //     println!("运行出错: {e}");
-    //     process::exit(1); //退出代码终止当前进程
-    // }
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        println!("输入错误: {err}");
+        process::exit(1); //退出代码终止当前进程
+    });
+	
+    if let Err(e) = minigrep::run(config) {
+        println!("运行出错: {e}");
+        process::exit(1); //退出代码终止当前进程
+    }
 }
