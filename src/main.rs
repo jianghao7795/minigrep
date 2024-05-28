0785b6e87e9c7e2168e51f993303ba8ee986bd8f
jianghao <jianghao@haplox.com>
2024-05-28 14:37:02 +0800

    fix: 更新注释

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,6 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    // let args = env::args(); // Args 实现了 Iterator
-    // let config = Config::build(args).unwrap();
-
-    // run(config).unwrap();
-    // unwrap_or_else 错误处理
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         println!("输入错误: {}", err);
         process::exit(1); //退出代码终止当前进程
